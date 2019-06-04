b684795d1d1fa609719328389228fd771f0e0e85
Nataliya R <n.rudenko@gmail.com>
2019-06-04 03:24:52 +0000

    Automatically backed up by Learn

--- db/migrate/20190604031732_create_users.rb
+++ db/migrate/20190604031732_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create table :users do |t|
       t.string :email 
-      t.string :password
+    #  t.string :password
   end 
   end
 end
