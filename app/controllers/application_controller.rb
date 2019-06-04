21d1a7577509cff17bad5caf44f017aa97772fcc
Nataliya R <n.rudenko@gmail.com>
2019-06-04 03:37:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     #your code here
      if !params[:username].empty? && !params[:password].empty?
     user = User.new(:username => params[:username], :password => params[:password])
-  if user.save
+  #if user.save
     redirect "/login"
   else
     redirect "/failure"
